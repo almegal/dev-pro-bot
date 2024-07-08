f42a15a7b83dd59ecb8842a4ac69ceed15125e63
alekseipeikhel <alekseipeikhel@gmail.com>
2024-07-08 21:54:34 +0300

    initial commit

---
+++ src/main/java/com/example/dev_pro/DevProApplication.java
@@ -0,0 +1,13 @@
+package com.example.dev_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dev_pro/DevProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dev_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
