cb7805dc301e9cf72805196cf627a9faf12a5a3f
alekseipeikhel <alekseipeikhel@gmail.com>
2024-07-20 17:05:59 +0300

    реализован метод стартового сообщения, а также логика проверки обращался пользователь или нет

--- src/main/java/com/example/dev_pro/impl/CommandHandlerServiceImpl.java
+++ src/main/java/com/example/dev_pro/impl/CommandHandlerServiceImpl.java
@@ -1,24 +1,26 @@
 package com.example.dev_pro.impl;
 
 import com.example.dev_pro.config.TelegramBotConfiguration;
+import com.example.dev_pro.model.TelegramUser;
 import com.example.dev_pro.model.Volunteer;
 import com.example.dev_pro.service.CommandHandlerService;
+import com.example.dev_pro.service.TelegramUserService;
 import com.example.dev_pro.service.VolunteerService;
+import com.example.dev_pro.service.shelter.CatShelterService;
+import com.example.dev_pro.service.shelter.DogShelterService;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class CommandHandlerServiceImpl implements CommandHandlerService {
-
-    private final TelegramBot telegramBot;
-    private final TelegramBotConfiguration tBotConfig;
-    private final VolunteerService volunteerService;
 
     private static final String START_COM = "/start";
     private static final String INFO_COM = "/info";
@@ -26,27 +28,28 @@ public class CommandHandlerServiceImpl implements CommandHandlerService {
     private static final String REPORT_COM = "/report";
     private static final String CALL_VOLUNTEER = "/call";
 
+    private final TelegramBot telegramBot;
+    private final TelegramBotConfiguration tBotConfig;
+    private final VolunteerService volunteerService;
+    private final TelegramUserService telegramUserService;
+    private final CatShelterService catShelterService;
+    private final DogShelterService dogShelterService;
+
     /**
      * Метод обработка команды и возврат результата.
-     * @param chatId идентификатор чата.
+     *
      * @param text текст команды.
      * @return сообщение для отправки.
      */
-    public String handleCommand(Long chatId, String text) {
-        switch (text) {
-            case START_COM:
-                return tBotConfig.getStartMsg();
-            case INFO_COM:
-                return tBotConfig.getInfoMsg();
-            case TAKE_COM:
-                return tBotConfig.getTakeMsg();
-            case REPORT_COM:
-                return null; // Добавим процессе создание база данных!
-            case CALL_VOLUNTEER:
-                return tBotConfig.getCallVolunteerMsg();
-            default:
-                return tBotConfig.getErrorMsg();
-        }
+    public String handleCommand(String text) {
+        return switch (text) {
+            case START_COM -> tBotConfig.getStartMsg();
+            case INFO_COM -> tBotConfig.getInfoMsg();
+            case TAKE_COM -> tBotConfig.getTakeMsg();
+            case REPORT_COM -> null; // Добавим процессе создание база данных!
+            case CALL_VOLUNTEER -> tBotConfig.getCallVolunteerMsg();
+            default -> tBotConfig.getErrorMsg();
+        };
     }
 
     /**
@@ -56,6 +59,7 @@ public class CommandHandlerServiceImpl implements CommandHandlerService {
      * из базы данных будет получен список никнеймов волонтеров и отправлен пользователю, одновременно всем волонтерам
      * будет отправлено сообщение о вызове и никнейм и идентификатор чата данного пользователя.
      * В отсутствие приведенных выше условий пользователю отправляется ранее созданное ответное сообщение.
+     *
      * @param update сообщение / команда пользователя
      */
     @Override
@@ -63,7 +67,29 @@ public class CommandHandlerServiceImpl implements CommandHandlerService {
         Long chatId = update.message().chat().id();
         String userName = update.message().chat().username();
         String text = update.message().text();
-        String resultMsg = handleCommand(chatId, text);
+        String resultMsg = handleCommand(text);
+
+        // Получаем идентификатор пользователя
+        Long userId = update.message().from().id();
+        // Получаем пользователя телеграмм, если он не найден вернется новый
+        TelegramUser telegramUser = telegramUserService.getById(userId);
+        // Если такого пользователя нет, значит он ранее не обращался
+        if (telegramUser.getTelegramId() == null) {
+            // отправляем стартовое сообщение
+            sendStartMessage(chatId);
+            return;
+        }
+        // Если дошли до сюда значит пользователь уже обращался
+        // Получаем приют который выбрал пользователь
+        String shelter = telegramUser.getShelter();
+        // в зависимости от приюта уже вызываем соответствующий сервис для обработки команд
+        if (shelter.equalsIgnoreCase("cat")) {
+            catShelterService.handleUpdate(update);
+            return;
+        } else if (shelter.equalsIgnoreCase("dog")) {
+            dogShelterService.handleUpdate(update);
+            return;
+        }
 
         if (text.equals(CALL_VOLUNTEER)) {
             List<String> listNickNamesVolunteers = volunteerService.getListNickNamesOfVolunteers();
@@ -78,12 +104,26 @@ public class CommandHandlerServiceImpl implements CommandHandlerService {
 
     /**
      * Метод отправки сообщения.
+     *
      * @param chatId идентификатор чата.
-     * @param msg сообщение для отправки.
+     * @param msg    сообщение для отправки.
      */
     @Override
     public void sendMsg(Long chatId, String msg) {
         SendMessage sendMessage = new SendMessage(chatId, msg);
+        telegramBot.execute(sendMessage);
+    }
+
+    // Приветственное сообщение для  новых пользователей
+    private void sendStartMessage(Long chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, tBotConfig.getStartMsg());
+        // Установим кнопки для пользователя чтобы мог выбрать приют
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup(
+                new InlineKeyboardButton("Cat").callbackData("cat"),
+                new InlineKeyboardButton("Dog").callbackData("dog")
+        );
+        // записываем это в наше сообщение и отправляем
+        sendMessage.replyMarkup(inlineKeyboardMarkup);
         telegramBot.execute(sendMessage);
     }
 
