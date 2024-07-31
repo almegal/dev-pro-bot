f8346f04b8df05931d057af4786c608502dde987
Aleksey-0052 <136725827+Aleksey-0052@users.noreply.github.com>
2024-07-31 14:33:49 +0300

    Update DogShelterService.java
    Удалены комменты гитхаба

--- src/main/java/com/example/dev_pro/service/shelter/DogShelterService.java
+++ src/main/java/com/example/dev_pro/service/shelter/DogShelterService.java
@@ -3,8 +3,6 @@ package com.example.dev_pro.service.shelter;
 import com.example.dev_pro.component.Buttons;
 
 public interface DogShelterService extends ShelterService, Buttons {
- aleekky-feature
 
 
- dev
 }
