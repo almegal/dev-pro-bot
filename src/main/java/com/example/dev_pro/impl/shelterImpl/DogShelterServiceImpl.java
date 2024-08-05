e8cbc5c598861fa0f7378252cb0b8fab5c83e337
Aleksey-0052 <136725827+Aleksey-0052@users.noreply.github.com>
2024-08-05 17:58:22 +0300

    Update DogShelterServiceImpl.java
    Удалены лишние данные

--- src/main/java/com/example/dev_pro/impl/shelterImpl/DogShelterServiceImpl.java
+++ src/main/java/com/example/dev_pro/impl/shelterImpl/DogShelterServiceImpl.java
@@ -3,56 +3,33 @@ package com.example.dev_pro.impl.shelterImpl;
 import com.example.dev_pro.botapi.BotStateContextDogShelter;
 import com.example.dev_pro.botapi.BotStateDogShelter;
 import com.example.dev_pro.cache.impl.UserDataCacheDogShelter;
-import com.example.dev_pro.component.impl.ShelterKeyBoardsButtons;
 import com.example.dev_pro.config.TelegramBotConfiguration;
-import com.example.dev_pro.exception.InvalidInputException;
 import com.example.dev_pro.listener.TelegramBotListener;
-
-import com.example.dev_pro.model.TelegramUser;
-import com.example.dev_pro.model.Volunteer;
-import com.example.dev_pro.service.TelegramUserService;
-import com.example.dev_pro.service.VolunteerService;
 import com.example.dev_pro.service.shelter.ShelterService;
-import com.example.dev_pro.util.MessageUtil;
-import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.springframework.context.annotation.Lazy;
-
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 import static com.example.dev_pro.component.impl.ShelterKeyBoardsButtons.*;
-
 
 @Service
 public class DogShelterServiceImpl implements ShelterService {
 
     private final TelegramBotConfiguration tBotConfig;
-    private final TelegramBot telegramBot;
-    private final ShelterKeyBoardsButtons buttons;
     private final TelegramBotListener listener;
-    private final TelegramUserService telegramUserService;
-    private final VolunteerService volunteerService;
     private final UserDataCacheDogShelter userDataCache;
     private final BotStateContextDogShelter botStateContext;
 
-    public DogShelterServiceImpl(TelegramBotConfiguration tBotConfig, TelegramBot telegramBot,
-                                 ShelterKeyBoardsButtons buttons, @Lazy TelegramBotListener listener,
-                                 TelegramUserService telegramUserService, VolunteerService volunteerService,
+    public DogShelterServiceImpl(TelegramBotConfiguration tBotConfig, @Lazy TelegramBotListener listener,
                                  UserDataCacheDogShelter userDataCache, BotStateContextDogShelter botStateContext) {
         this.tBotConfig = tBotConfig;
-        this.telegramBot = telegramBot;
-        this.buttons = buttons;
         this.listener = listener;
-        this.telegramUserService = telegramUserService;
-        this.volunteerService = volunteerService;
         this.userDataCache = userDataCache;
         this.botStateContext = botStateContext;
     }
-
+    
     @Override
     public void handleUpdate(Update update) {
         Message message = update.message();
