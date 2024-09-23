397a3a9e37f5ad2233fba8bff1de01e2ab8151b4
KosbakovIsmail <kosbakovismail@gmail.com>
2024-09-23 13:30:06 +0300

    Добавление Описание в PetController           изменения паттерна класса MessageUtil       Добавления класса changeLog-master.yml

--- src/main/java/com/example/dev_pro/controller/PetController.java
+++ src/main/java/com/example/dev_pro/controller/PetController.java
@@ -3,6 +3,7 @@ package com.example.dev_pro.controller;
 import com.example.dev_pro.dto.PetDto;
 import com.example.dev_pro.model.Pet;
 import com.example.dev_pro.service.PetService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,42 +21,76 @@ public class PetController {
     private final PetService petService;
 
     @PostMapping
+    @Operation(
+            summary = "Создание питомца",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Создаваемый питомец. При создании питомца и сохранении его в базу данных " +
+                            "переданный идентификатор будет проигнорирован, и питомцу будет присвоен идентификатор, " +
+                            "следующий за идентификатором последнего питомца в базе данных"
+            )
+    )
     public ResponseEntity<Pet> createPet(@RequestBody PetDto petDto) {
         Pet createdPet = petService.createPet(petDto);
         return ResponseEntity.ok(createdPet);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Обновление питомца",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    description = "Обновляемый питомец. Данные о питомце изложены в формате JSON"
+            )
+    )
     public ResponseEntity<Pet> updatePet(@PathVariable Long id, @RequestBody PetDto petDto) {
         Pet updatedPet = petService.updatePet(id, petDto);
         return ResponseEntity.ok(updatedPet);
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Получение питомца по идентификатору",
+            description = "Выгрузка питомца по его идентификатору"
+    )
     public ResponseEntity<Pet> getPetById(@PathVariable("id") Long id) {
         Pet pet = petService.findPetById(id);
         return ResponseEntity.ok(pet);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Удаление питомца по идентификатору",
+            description = "Удаление питомца из базы данных по его идентификатору"
+    )
     public ResponseEntity<Pet> deletePetById(@PathVariable("id") Long id) {
         Pet pet = petService.deletePetById(id);
         return ResponseEntity.ok(pet);
     }
 
     @GetMapping("/all")
+    @Operation(
+            summary = "Получение всех питомцев",
+            description = "Получение списка всех питомцев из базы данных"
+    )
     public ResponseEntity<Collection<Pet>> getAllPets() {
         Collection<Pet> pets = petService.findAllPets();
         return ResponseEntity.ok(pets);
     }
 
     @GetMapping("/all-pets-by-shelter/{shelterId}")
+    @Operation(
+            summary = "Получение всех питомцев по идентификатору приюта",
+            description = "Получение списка всех питомцев, находящихся в указанном приюте"
+    )
     public ResponseEntity<List<Pet>> getAllByShelterId(@PathVariable("shelterId") Integer shelterId) {
         List<Pet> pets = petService.findAllByShelterId(shelterId);
         return ResponseEntity.ok(pets);
     }
 
     @GetMapping("/all-free-pets-by-shelter")
+    @Operation(
+            summary = "Получение всех свободных питомцев по идентификатору приюта",
+            description = "Получение списка всех свободных питомцев, находящихся в указанном приюте"
+    )
     public ResponseEntity<List<Pet>> getAllByShelterIdAndIsFreeStatus(
             @RequestParam Integer shelterId,
             @RequestParam boolean isFreeStatus
@@ -65,6 +100,10 @@ public class PetController {
     }
 
     @GetMapping("/all-pets-by-adopter/{adopterId}")
+    @Operation(
+            summary = "Получение всех питомцев по идентификатору усыновителя",
+            description = "Получение списка всех питомцев, закрепленных за указанным усыновителем"
+    )
     public ResponseEntity<List<Pet>> getAllByAdopterId(@PathVariable("adopterId") Long adopterId) {
         List<Pet> pets = petService.findAllByAdopterId(adopterId);
         return ResponseEntity.ok(pets);
--- src/main/java/com/example/dev_pro/util/MessageUtil.java
+++ src/main/java/com/example/dev_pro/util/MessageUtil.java
@@ -9,15 +9,13 @@ import java.util.regex.Pattern;
 
 public class MessageUtil {
 
-    public static final Pattern pattern = Pattern.compile("([А-я]+)(\\s)([А-я]+)(\\s)([А-я]+)(\\s)" +
-            "(\\d{10})(\\s)([А-я0-9]+)");
-
+    public static final Pattern pattern = Pattern.compile("([А-Яа-яЁё]+)(\\s)([А-Яа-яЁё]+)(\\s)([А-Яа-яЁё]+)(\\s)(\\d{10})(\\s)([А-Яа-яA-Za-z0-9]+)");
 
     /**
      * Распарсиваем строку, поступившую от пользователя в формате "Иванов Иван Иванович 9052341234 а123bc".
-     * Первая, третья и пятая части паттерна описывают фамилию, имя и отчество: ([А-я]+).
+     * Первая, третья и пятая части паттерна описывают фамилию, имя и отчество: ([А-Яа-яЁё]+).
      * Седьмая часть паттерна описывает номер мобильного телефона: (\\d{10}).
-     * Девятая часть паттерна описывает номер автомобиля: ([А-я0-9]+).
+     * Девятая часть паттерна описывает номер автомобиля: ([А-Яа-яA-Za-z0-9]+).
      * Четные части паттерна описывают пробел: (\\s).
      * Создаем объект типа Matcher для выполнения операций поиска по шаблону.
      * Проверяем, подходит ли строка под паттерн: matcher.find().
@@ -33,10 +31,13 @@ public class MessageUtil {
                 telegramUser.setPhoneNumber(matcher.group(7));
                 telegramUser.setCarNumber(matcher.group(9));
                 return telegramUser;
+            } else {
+                System.err.println("Pattern not matched for command: " + command);
             }
+        } else {
+            System.err.println("Command is empty or null: " + command);
         }
 
         throw new InvalidInputException("Incorrect command: " + command);
     }
-
 }
